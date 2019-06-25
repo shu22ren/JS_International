5e4aa177f307bfbac96421a92fd65b60196df526
arronhai <arron.hu@cn-unisoft.com>
2019-06-25 11:01:01 +0800

    增加越南SPR

--- write_json.py
+++ write_json.py
@@ -15,6 +15,8 @@ def write_json(language,container,role,path):
         url = 'https://exeutest.blob.core.chinacloudapi.cn/%s/sfdc-verson_international.json' %container
     elif language == 'india':
         url = 'https://exeutest.blob.core.chinacloudapi.cn/%s/ontap_in_versions.json' % container
+    elif language == 'VN':
+        url = 'https://exeutest.blob.core.chinacloudapi.cn/%s/sfdc-verson-vn.json' % container
     else:
         url = 'https://exeutest.blob.core.chinacloudapi.cn/%s/sfdc-verson.json' % container
 
@@ -26,6 +28,8 @@ def write_json(language,container,role,path):
         roles = ['CN BDR']
     elif role == 'Inspection':
         roles = ['Inspection']
+    elif role == 'VNSPR':
+        roles = ['VN SPR']
 
     # 读取URL中的json文件
     text = json.loads(requests.get(url).text)
@@ -54,4 +58,4 @@ def write_json(language,container,role,path):
             fw.write(json.dumps(text, indent=4))
 
 write_json(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
-# write_json('international','sfdc-test','bdr',r'C:/Users/72941/Desktop/t.json')
+# write_json('VN','sfdc-test','VNSPR',r'C:/Users/72941/Desktop/t.json')
