cd7e446f841eb727b45ce39247c78a4503b16241
shu22ren <liangshuang22@126.com>
2019-07-09 15:56:27 +0800

    Update write_json.py
    调用

--- write_json.py
+++ write_json.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-import requests
 import re
 import json
+import requests
 import sys
 
 
@@ -11,6 +11,7 @@ import sys
 re_str = re.compile(r"'js_version': '1.0.(.*?)'")
 
 def write_json(language,container,role,path):
+    print('role==='+role)
     if language == 'international':
         url = 'https://exeutest.blob.core.chinacloudapi.cn/%s/sfdc-verson_international.json' %container
     elif language == 'india':
@@ -30,12 +31,17 @@ def write_json(language,container,role,path):
         roles = ['Inspection']
     elif role == 'VNSPR':
         roles = ['VN SPR']
-
+    elif role.index("|")!=-1:
+        roles = role.split("|")
+    else:
+        roles = [role]
+    #print(roles)
     # 读取URL中的json文件
     text = json.loads(requests.get(url).text)
-    info = text.get('app_info')
     for role in roles:
+        info = text.get('app_info')
         js = str(info[role])
+        #print(role+'=='+js)
         s = int(re_str.findall(js)[0])
         # 针对补丁版本号的位数进行处理
         if s//10 == 0:
@@ -53,9 +59,10 @@ def write_json(language,container,role,path):
         new_json = eval(re_str.sub(re_s,js,1))
         # 修改json文件中对应角色的版本号
         text['app_info'][role] = new_json
-        # 读取到的内容写入json中
-        with open(path,'w') as fw:
-            fw.write(json.dumps(text, indent=4))
+
+    # 读取到的内容写入json中
+    with open(path,'w',encoding='utf-8') as fw:
+        fw.write(json.dumps(text,ensure_ascii=False, indent=4) )
 
 write_json(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
-# write_json('VN','sfdc-test','VNSPR',r'C:/Users/72941/Desktop/t.json')
+#write_json('VN','sfdc-test','VN BDR|VN M1',r'D:\python_work\sfdc-verson-vn.json')
